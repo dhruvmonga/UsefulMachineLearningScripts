27e432bc60e69a895ec46d939cb22bf77a378605
Dhruv Monga <dhruvmonga@gmail.com>
2019-01-25 11:27:45 -0800

    removed sampling

--- ModelInterpreter.py
+++ ModelInterpreter.py
@@ -5,11 +5,10 @@ from pdpbox import info_plots, pdp
 import matplotlib.pyplot as plt
 
 class ModelInterpreter():
-    def __init__(self, model, feature_names, X_train, sample_size):
+    def __init__(self, model, feature_names, X_train):
         self.model = model
         self.feature_names = feature_names
         self.X_train = X_train
-        self.sample_size = sample_size
     
     def plot_weights(self,num_weights=None):
         display(eli5.show_weights(self.model,top=num_weights,feature_names=self.feature_names))
@@ -19,29 +18,29 @@ class ModelInterpreter():
         shap.initjs()
         self.explainer = None
         if model_type == 'linear':
-            self.explainer = shap.LinearExplainer(self.model,self.X_train.sample(self.sample_size,random_state=42))
+            self.explainer = shap.LinearExplainer(self.model,self.X_train)
         elif model_type == 'tree':
-            self.explainer = shap.TreeExplainer(self.model,self.X_train.sample(self.sample_size,random_state=42))
+            self.explainer = shap.TreeExplainer(self.model,self.X_train)
         else:
-            self.explainer = shap.KernelExplainer(self.model,self.X_train.sample(self.sample_size,random_state=42))
-        shap_values = self.explainer.shap_values(self.X_train.sample(self.sample_size,random_state=42))
-        display(shap.summary_plot(shap_values,self.X_train.sample(self.sample_size,random_state=42)))
-        display(shap.summary_plot(shap_values,self.X_train.sample(self.sample_size,random_state=42),plot_type='bar'))
+            self.explainer = shap.KernelExplainer(self.model,self.X_train)
+        shap_values = self.explainer.shap_values(self.X_train)
+        display(shap.summary_plot(shap_values,self.X_train))
+        display(shap.summary_plot(shap_values,self.X_train,plot_type='bar'))
 
     def show_SHAP_PDP(self, features=[]):
         for f in features:
             if len(f) > 1:
                 continue
             shap.dependence_plot(f, 
-                                 self.explainer.shap_values(self.X_train.sample(self.sample_size,random_state=42)), 
-                                 self.X_train.sample(self.sample_size,random_state=42))
+                                 self.explainer.shap_values(self.X_train), 
+                                 self.X_train)
             
     def show_SHAP_PDP_interaction(self, features=[]):
         for f in features:
             try:
                 shap.dependence_plot(tuple(f), 
-                                 self.explainer.shap_interaction_values(self.X_train.sample(self.sample_size,random_state=42)), 
-                                 self.X_train.sample(self.sample_size,random_state=42))
+                                 self.explainer.shap_interaction_values(self.X_train), 
+                                 self.X_train)
             except:
                 print("Linear estimators don't have interaction values.")
                 return
@@ -51,14 +50,14 @@ class ModelInterpreter():
             print("features and feature names must have same size")
             return
         for f,n in zip(features,feature_names):
-            info_plots.actual_plot(self.model,self.X_train.sample(self.sample_size,random_state=42),
+            info_plots.actual_plot(self.model,self.X_train,
                       feature=f,feature_name=n,predict_kwds={})
             plt.xticks(rotation=90)
             plt.show()
     
     def show_PDP_isolate(self, features=[]):
         for f in features:
-            pdp_isolate = pdp.pdp_isolate(self.model,self.X_train.sample(self.sample_size,random_state=42),
+            pdp_isolate = pdp.pdp_isolate(self.model,self.X_train,
                       model_features=self.feature_names,feature=f,predict_kwds={})
             pdp.pdp_plot(pdp_isolate,feature_name=f)
             plt.xticks(rotation=90)
@@ -66,7 +65,7 @@ class ModelInterpreter():
     
     def show_PDP_interact(self, features=[]):
         for f in features:
-            pdp_interact = pdp.pdp_interact(self.model,self.X_train.sample(self.sample_size,random_state=42),
+            pdp_interact = pdp.pdp_interact(self.model,self.X_train,
                                    model_features=self.feature_names,
                                             features=f)
             pdp.pdp_interact_plot(pdp_interact,feature_names=f)
